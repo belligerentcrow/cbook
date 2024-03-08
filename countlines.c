712143034174af2db913fffe615c5a7d054199d2
belligerentcrow <deadblink333@gmail.com>
2024-03-08 20:29:07 +0100

    more basics

---
+++ countlines.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int count, nspace;
+    nspace = 0;
+    while((count = getchar())!= EOF){
+        if (c == ' ')
+            ++nspace;
+    }
+    printf("%d\n", nl);
+}
---
+++ power.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+    int first, second;
+    first = 10;
+    second = 3;
+    int powpow = power(first, second);
+    printf("%d to the %d = %d\n", first, second, powpow);
+}
+
+int power(int x, int y){
+    int poweringup = 1;
+    for(int i = 0; i <y; ++i){
+        poweringup = poweringup*x;
+    }
+    return poweringup;
+}
