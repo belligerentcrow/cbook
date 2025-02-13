4f51c4ce36c6c6b4a52889c9e45e09440dddfa15
belligerentcrow <74275837+belligerentcrow@users.noreply.github.com>
2025-02-13 22:12:12 +0100

    update

---
+++ chapt1/fahrenheit_to_celsius1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+main (){
+    int fahr, celsius; //variables for the temperatures
+    int lower, step, upper; 
+
+    lower = 0; //starting point
+    upper = 300; //ending point
+    step = 20; //how much is added each step
+
+    fahr = lower; //we start the fahr counting with 0
+    while(fahr<=upper){   //while the fahr temperature is not yet 300;
+        celsius = 5* (fahr - 32) / 9;  //cels to fahr conversion formula
+        printf("%d \t %d\n", fahr, celsius); // print fahr then cels
+        fahr = fahr + step; //add the step to fahr temperature
+    }
+}
---
+++ chapt1/hello_world_again.c
@@ -0,0 +1,6 @@
+#include <stdio.h> //include the standard library i/o
+
+main (){  //define a function named main that receives no argument values
+    printf("hello, world\n"); 
+    //main calls library function printf, to print this sequence of characters: \n represents the newline character (enter) 
+}
