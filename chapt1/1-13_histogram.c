23850f1cb510fdcdfbfd7f45efd6320ad511d2dc
belligerentcrow <74275837+belligerentcrow@users.noreply.github.com>
2025-02-18 12:10:22 +0100

    up

--- chapt1/1-13_histogram.c
+++ chapt1/1-13_histogram.c
@@ -4,10 +4,21 @@
 #define OUT 0
 
 main(){
-    int wlenghts[20];
-    int lenghtword;
-    int c;
-    while() {
+    int wlenghts[30]; //array with 'all' possible lenghts
+    int state, lenghtword, c, prevstate; 
+    prevstate = OUT;
+    state = lenghtword = OUT; //state notes if we're in a word, while lenghtword counts the lenght
 
+    while((c = getchar()) != '0') { // 0 as stop key
+        ++lenghtword;       //every char it increases
+        if(c == ' ' || c == '\n' || c == '\t'){
+            state = OUT;
+            if(prevstate ==OUT){
+            ++wlenghts[lenghtword+1]; //increases the corresponding place in the array
+            } 
+            lenghtword = 0; //then sets the current word lenght to 0
+        }else if (state == OUT){
+            state = IN;
+        }
     }
 }
