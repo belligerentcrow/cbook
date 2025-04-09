47ffdbc67911b58a0ffebc7f06249a4cbc52a146
belligerentcrow <74275837+belligerentcrow@users.noreply.github.com>
2025-04-09 19:40:18 +0200

    Comment sockets

--- ntwrk/QOTD/UDPServer.c
+++ ntwrk/QOTD/UDPServer.c
@@ -13,7 +13,10 @@
 
 
 int main(int argc, char * argv[]){
-    srand(time(0));
+
+    srand(time(0)); //this lets the seed of randomness be defined through clock: offering a better randomization 
+
+    //quotes list
     const char * quotes[QUOTESNUMBER] ={
         "Laugh hard, run fast, be kind --Twelfth Doctor",
     "The lieutenant remains silent, but his expression couldn't say I told you so any louder. --Kim Kitsuragi",
@@ -28,29 +31,39 @@ int main(int argc, char * argv[]){
 
     socklen_t addr_len = sizeof(client_addr);
     
+    //clearing up memory before using it
     memset(&server_addr, 0, addr_len);
     memset(&client_addr, 0, sizeof(client_addr));
+    memset(client_request, 0, sizeof(client_request));
+
+    //structuing the socket
     server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(20300);
+    server_addr.sin_port = htons(20300); //arbitrary hardcoded port
     server_addr.sin_addr.s_addr = INADDR_ANY; //accetta da qualsiasi indirizzo
     
     int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
     printf("OK Socket\n");
 
+    //binding the socket
     bind(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr));
     printf("OK Bind\n");
+    
+    //this defines the length of the client address in socklen_t format 
     socklen_t cl_addr_len = sizeof(client_addr);
+
+    //the receive from: the client is listening
     recvfrom(sockfd, client_request,sizeof(client_request), 0, (struct sockaddr *)&client_addr, &cl_addr_len);
+
+    //acknowledges connection
     printf("Received connection from %s", inet_ntoa(client_addr.sin_addr));
 
-    
+    //picks a random number from 0 to number-of-quotes 
     int quotenumb = (rand()%QUOTESNUMBER);
 
-    
+    //sends the quote
     sendto(sockfd, quotes[quotenumb], sizeof(quotes[quotenumb]), 0, (struct sockaddr *)&client_addr,cl_addr_len);
-    for(int i = 0; i < 2000; ++i){
-
-    }
+    
+    //closes the socket immediately after
     close(sockfd);
     return 0;
     
