95680ac7a2ed23b93b9678003a4b0f3db8690c5f
belligerentcrow <74275837+belligerentcrow@users.noreply.github.com>
2025-04-20 22:25:40 +0200

    Update to step 19

--- TextEditor/kilo.c
+++ TextEditor/kilo.c
@@ -1,19 +1,32 @@
-//step 15
+//step 19
 
+/*** includes ***/
 #include <ctype.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
+/*** data ***/
 struct termios orig_termios;
 
+
+/*** terminal ***/
+
+//error handling
+void die(const char *s){
+    perror(s);
+    exit(1);
+}
+
+
 void disableRawMode(){
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1){die("tcsetattr");}
 }
 
 void enableRawMode(){
-    tcgetattr(STDIN_FILENO,&orig_termios);
+    if(tcgetattr(STDIN_FILENO,&orig_termios)==-1){ die("tcgetattr"); }
     atexit(disableRawMode); //calls disable raw mode on program exit
     
     struct termios raw = orig_termios;
@@ -29,22 +42,26 @@ void enableRawMode(){
     //the not operator lets program filter out these flags.
     //IEXTEN blocks ctrl-v, control character
     raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG); //local flags. echo is a bitflag-> with NOT operator
+    //control characters
+    raw.c_cc[VMIN]=0;
+    raw.c_cc[VTIME] =1;
 
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw)==-1){die("tcsetattr");};
 }
 
+/*** init ***/
 
 int main(){
     enableRawMode();
-    char c;
-    
-    while(read(STDIN_FILENO, &c, 1)==1 && c!='q'){
-        if(iscntrl(c)){ //tests if it is a control character
+    while(1){
+        char c = '\0';
+        if(read(STDIN_FILENO, &c, 1) == -1 && errno !=EAGAIN){die("read");}
+        if(iscntrl(c)){
             printf("%d\r\n",c); 
         }else{
             printf("%d ('%c')\r\n",c,c);
         }
+        if(c == 'q') break;
     }
-
     return 0;
 }
