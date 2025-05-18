054c35fe2577342b9979a20267fed8791bd29531
belligerentcrow <74275837+belligerentcrow@users.noreply.github.com>
2025-05-18 15:06:21 +0200

    add c notes

---
+++ learnC/v_1_opt2.s
@@ -0,0 +1,26 @@
+	.file	"v_1_opt2.c"
+	.text
+	.section	.rodata.str1.1,"aMS",@progbits,1
+.LC0:
+	.string	"Hello, World!"
+	.section	.text.startup,"ax",@progbits
+	.p2align 4
+	.globl	main
+	.type	main, @function
+main:
+.LFB11:
+	.cfi_startproc
+	subq	$8, %rsp
+	.cfi_def_cfa_offset 16
+	leaq	.LC0(%rip), %rdi
+	xorl	%eax, %eax
+	call	printf@PLT
+	xorl	%eax, %eax
+	addq	$8, %rsp
+	.cfi_def_cfa_offset 8
+	ret
+	.cfi_endproc
+.LFE11:
+	.size	main, .-main
+	.ident	"GCC: (Debian 12.2.0-14+deb12u1) 12.2.0"
+	.section	.note.GNU-stack,"",@progbits
