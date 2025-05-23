5baf43dec13f9eb4ad0359e19b73d9de17a22740
belligerentcrow <74275837+belligerentcrow@users.noreply.github.com>
2025-05-24 01:19:26 +0200

    Update copyc.c

--- Sistemi/copyc.c
+++ Sistemi/copyc.c
@@ -8,30 +8,34 @@
 #define BUFSIZE 8192
 
 int main(int argc,  char * argv[]){
-	int sd, dd, size;
+	int sd, dd, size, total=0, pos;
 	char buffer[BUFSIZE];
 	if(argc!=3){
 		printf("usage: copy <source> <dest>");
 		exit_with_sys_err("wrong args");
 	}
-	
-	if((sd = open(argv[1], O_RDONLY))<0){
+	if((sd = open(argv[1], O_RDONLY, S_IRUSR | S_IRGRP))<0){
 		exit_with_sys_err("open source");
+	}else{
+		pos = lseek(sd, 0, SEEK_END);
+		printf("[MAIN] lettura di '%s', di %d byte\n", argv[1], pos);
+		pos = lseek(sd, 0, SEEK_SET);
 	}
-	if((dd=open(argv[2], O_WRONLY | O_CREAT))<0){
+	if((dd=open(argv[2], O_WRONLY | O_CREAT, S_IWUSR | S_IWGRP))<0){
 		exit_with_sys_err("open or create destination");
 	}
-	
+	printf("[MAIN] duplicazione di %d file\n",argc-2);
 	do{
 		if((size=read(sd, buffer, BUFSIZ))<0){
 			exit_with_sys_err("open from source copy");	
 		}
+		total+=size;
+		printf("Ho letto %d byte\n", size);
 		if((write(dd, buffer, size))<0){
 			exit_with_sys_err("write on destination");
 		}
-
 	}while(size==BUFSIZ);
-
+	printf("La dimensione totale è di %d byte\n",total);
 	close(sd);
 	close(dd);
 	exit(EXIT_SUCCESS);
